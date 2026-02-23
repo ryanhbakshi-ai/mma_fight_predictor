f4493a0e762c35bd0b9b1c1bd0e1718910ac4ff0
Ryan Bakshi <ryanhbakshi@gmail.com>
2026-02-23 17:57:36 -0500

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-def removeDuplicates(self, nums: list[int]) -> int:
-    for i in range: nums.length
-    j=i+1
-    for j in range: nums.length
-    if nums[i]==nums[j]:
-        nums.remove[i]
-    return nums.length
